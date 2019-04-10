5d42cb6c5968f6d9e3dcdd67dd669028d032df5b
Kyrie Lee <lzhsummer@gmail.com>
2019-04-10 16:53:56 +1000

    slightly change: fetch test

--- Assn3/frontend/heart-disease/src/components/ChartDemo.js
+++ Assn3/frontend/heart-disease/src/components/ChartDemo.js
@@ -3,12 +3,20 @@ import CanvasJSReact from './canvasjs.react';
 var CanvasJSChart = CanvasJSReact.CanvasJSChart;
 
 export default class ChartDemo extends Component {
+
+  getData = async () => {
+    const res = await fetch('http://localhost:5000/collections')
+    const data = await res.json()
+    console.log(data[0])
+    console.log(data[0].age)
+  } 
+
   render() {
     const options = {
       title: {
         text: "Basic Column Chart in React"
-      },
-      data: [{				
+      },      
+      data: [{				  
                 type: "column",
                 dataPoints: [
                     { label: "Apple",  y: 10  },
@@ -25,6 +33,7 @@ export default class ChartDemo extends Component {
         <CanvasJSChart options = {options}
             /* onRef = {ref => this.chart = ref} */
         />
+        <button onClick={this.getData}>click to test</button>
       </div>
     );
   }
